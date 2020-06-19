81f33bd708dbbe5f0ca0eb8c44b1a864e629e2f6
eani <marceloandrioni@gmail.com>
2020-06-19 19:49:15 -0300

    some new ESRI layers

--- cartopy_extra_tiles/esri_tiles.py
+++ cartopy_extra_tiles/esri_tiles.py
@@ -9,9 +9,11 @@
 import cartopy.crs as ccrs
 from cartopy.io.img_tiles import GoogleTiles
 
+
 class ESRI(GoogleTiles):
-    """ESRI imagery, shaded relief, street map, terrain, topo, and
-    National Geographic tiles.
+    """ESRI imagery, shaded relief, street map, terrain, topo,
+        National Geographic, dark, dark-labels, gray, gray-labels, ocean,
+        ocean-labels, labels1, labels2 and labels3 tiles.
     """
 
     def __init__(self, desired_tile_form="RGB", style="standard"):
@@ -22,8 +24,10 @@ class ESRI(GoogleTiles):
         CMYK, HSV, I, F)
 
         :type style: str
-        :param style: Stamen flavor
-            ("imagery", "shadedrelief", "streetmap", "terrain", "topo", "natgeo")
+        :param style: ESRI flavor
+            ("imagery", "shadedrelief", "streetmap", "terrain", "topo", "natgeo",
+             "dark", "dark-labels", "gray", "gray-labels", "ocean", "ocean-labels",
+             "labels1", "labels2", "labels3")
         """
         styles = {
             "imagery": "World_Imagery",
@@ -32,7 +36,16 @@ class ESRI(GoogleTiles):
             "terrain": "World_Terrain_Base",
             "topo": "World_Topo_Map",
             "natgeo": "NatGeo_World_Map",
-            }
+            "dark": "Canvas/World_Dark_Gray_Base",
+            "dark-labels": "Canvas/World_Dark_Gray_Reference",
+            "gray": "Canvas/World_Light_Gray_Base",
+            "gray-labels": "Canvas/World_Light_Gray_Reference",
+            "ocean": "Ocean/World_Ocean_Base",
+            "ocean-labels": "Ocean/World_Ocean_Reference",
+            "labels1": "Reference/World_Boundaries_and_Places",
+            "labels2": "Reference/World_Boundaries_and_Places_Alternate",
+            "labels3": "Reference/World_Reference_Overlay",
+        }
         if style not in styles:
             msg = "Invalid style '%s'. Valid styles: %s" % (style, ", ".join(styles.keys()))
             raise ValueError(msg)
--- test/test_esri_tiles.py
+++ test/test_esri_tiles.py
@@ -10,6 +10,7 @@
 import sys
 
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 sys.path.append("../cartopy_extra_tiles")
 from cached_tiler import CachedTiler
@@ -22,10 +23,27 @@ styles = (
     "terrain",
     "topo",
     "natgeo",
+    ["dark", "dark-labels"],
+    ["gray", "gray-labels"],
+    ["ocean", "ocean-labels"],
+    ["imagery", "labels1"],
+    ["imagery", "labels2"],
+    ["imagery", "labels3"],
     )
+
 for style in styles:
-    tiler = CachedTiler(ESRI(style=style), cache_dir="~/data_scratch/images/tiles")
-    ax = plt.axes(projection=tiler.crs)
+
+    fig, ax = plt.subplots(1, 1, subplot_kw={'projection': ccrs.PlateCarree()})
     ax.set_extent([-123, -121.5, 37, 38.5])
-    ax.add_image(tiler, 8)
+
+    style = [style] if isinstance(style, str) else style
+
+    for s in style:
+        tiler = CachedTiler(ESRI(desired_tile_form='RGBA', style=s),
+                            cache_dir="~/data_scratch/images/tiles")
+        ax.add_image(tiler, 8)
+
+    ax.set_title(style)
     plt.show()
+
+    fig.savefig('/tmp/example_' + '_'.join(style) + '.png', bbox_inches='tight')
