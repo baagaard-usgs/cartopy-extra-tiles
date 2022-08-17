99c9af53d9a06f346344d44e54cf4a12b3b695b9
Brad Aagaard <baagaard@usgs.gov>
2022-08-17 15:52:58 -0600

    Add more ESRI tile formats (contributed by Marcelo Andrioni).

--- cartopy_extra_tiles/esri_tiles.py
+++ cartopy_extra_tiles/esri_tiles.py
@@ -2,6 +2,8 @@
 #
 # Brad T. Aagaard, U.S. Geological Survey
 #
+# Additional tile format setup contributed by Marcelo Andrioni.
+#
 # ======================================================================
 """Additional tiles from ESRI.
 """
@@ -10,8 +12,8 @@ import cartopy.crs as ccrs
 from cartopy.io.img_tiles import GoogleTiles
 
 class ESRI(GoogleTiles):
-    """ESRI imagery, shaded relief, street map, terrain, topo, and
-    National Geographic tiles.
+    """ESRI imagery, shaded relief, street map, terrain, topo, dar, gray,
+    ocean, labels and National Geographic tiles.
     """
 
     def __init__(self, desired_tile_form="RGB", style="standard"):
@@ -23,7 +25,9 @@ class ESRI(GoogleTiles):
 
         :type style: str
         :param style: Stamen flavor
-            ("imagery", "shadedrelief", "streetmap", "terrain", "topo", "natgeo")
+            ("imagery", "shadedrelief", "streetmap", "terrain", "topo", "natgeo",
+            "dark", "dark-labels", "gray", "gray-labels", "ocean", "ocean-labels",
+            "world-labels", "world-alt-labels", "world-overlay")
         """
         styles = {
             "imagery": "World_Imagery",
@@ -32,6 +36,16 @@ class ESRI(GoogleTiles):
             "terrain": "World_Terrain_Base",
             "topo": "World_Topo_Map",
             "natgeo": "NatGeo_World_Map",
+            "dark": "Canvas/World_Dark_Gray_Base",
+            "dark-labels": "Canvas/World_Dark_Gray_Reference",
+            "gray": "Canvas/World_Light_Gray_Base",
+            "gray-labels": "Canvas/World_Light_Gray_Reference",
+            "ocean": "Ocean/World_Ocean_Base",
+            "ocean-labels": "Ocean/World_Ocean_Reference",
+            "world-labels": "Reference/World_Boundaries_and_Places",
+            "world-alt-labels": "Reference/World_Boundaries_and_Places_Alternate",
+            "world-overlay": "Reference/World_Reference_Overlay",
+
             }
         if style not in styles:
             msg = "Invalid style '%s'. Valid styles: %s" % (style, ", ".join(styles.keys()))
